8b04942e6e5d9f50a60385166d9d82030e860b4c
kam <148465412+raspy-windbird@users.noreply.github.com>
2025-09-14 17:11:16 +0900

    更新 pxt.json, main.ts

---
+++ main.ts
@@ -0,0 +1,25 @@
+//% weight=100 color=#0080ff icon="\uf1b2"
+namespace MyGrove {
+
+//% block="ブート音を再生"
+export function Boot() {
+    music.play(music.createSoundExpression(
+        WaveShape.Square,72,98,3,4,5000,
+        SoundExpressionEffect.None,InterpolationCurve.Linear
+    ), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(
+        WaveShape.Square,98,98,5,5,2000,
+        SoundExpressionEffect.None,InterpolationCurve.Linear
+    ), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(
+        WaveShape.Square, 2000,2000,25,25,150,
+        SoundExpressionEffect.None,InterpolationCurve.Logarithmic
+    ), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(
+        WaveShape.Square,1000,1000,25,25,200,
+        SoundExpressionEffect.None,InterpolationCurve.Linear
+    ), music.PlaybackMode.UntilDone)
+}
+
+
+}
